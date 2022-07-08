cc8fb50e9188de79c8c19ff7aa99768d854932cd
JeffersonRSoares <jefferson.silva15@aluno.senai.br>
2022-07-08 11:00:10 -0300

    atualizando lista de exercicios

---
+++ ExerciciosProfRomulo.ExercicioOito/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.ExercicioOito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Exercício 8
+             ======================
+
+             * Crie um programa que entre com um número inteiro
+                e mostre o fatorial desse número.
+            Exemplo:
+            5! = 120
+            5*4*3*2*1=  120
+            */
+            int i, number, fact;
+            Console.WriteLine("Digite um número");
+            number = int.Parse(Console.ReadLine());
+            fact = number;
+            for (i = number - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFatorial do número dado é?: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExerciciosProfRomulo.ExercicioSete/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.ExercicioSete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             *Exercício 7
+            ================================================
+            Mostrar os números de 100 até 1 de forma decrescen
+            */
+            int i = 100;
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                //i=i-i;
+                i--;
+            }
+        
+        Console.ReadKey();
+          }
+    }
+}
